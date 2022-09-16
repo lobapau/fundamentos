1a02c188456d1effb7da0e3d1cb21c10a7600c2f
Pauloba Colobon <113697732+lobapau@users.noreply.github.com>
2022-09-16 13:45:39 -0500

    Update suma.cpp

--- suma.cpp
+++ suma.cpp
@@ -1,2 +1,15 @@
+# incluir < iostream >
+utilizando el espacio de  nombres  estándar ;
 
-baaaaa
+int  principal ()
+{
+
+ flotante x, y, z;
+cout<< " Ingreso 2 numero xy: " ;
+cin>>x>>y;
+z=x+y;
+cout<< " El resultado es: " ;
+cout<<z<<endl;
+
+ devolver  0 ;
+}
