c455a84a6a6af4f6f7ece797a6e51254c143448b
Pauloba Colobon <113697732+lobapau@users.noreply.github.com>
2022-09-16 13:51:29 -0500

    Create calculadora.cpp

---
+++ calculadora.cpp
@@ -0,0 +1,24 @@
+# incluir < iostream >
+utilizando el espacio de  nombres  estándar ;
+
+int  principal ()
+{
+	flotante x,y,s,m,d,r;
+	// Ingreso de datos
+	cout<< " Ingreso en valor de x=: " ;
+	cin>>x;
+	cout<< " Ingreso en valor de y=: " ;
+	// Operaciones
+	cin>>y;
+	s=x+y;
+	m=x*y;
+	d=x/y;
+	r=xy;
+	// Muestra los resultados.
+	cout<< " Las sumas de " <<x<< " + " <<y<< " = " <<s<<endl;
+	cout<< " Las multiplicaciones de " <<x<< " * " <<y<< " = " <<m<<endl;
+	cout<< " Las divisiones de " <<x<< " / " <<y<< " = " <<d<<endl;
+	cout<< " Las resta de " <<x<< " - " <<y<< " = " <<r<<endl;
+	devolver  0 ;
+
+}
