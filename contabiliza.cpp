f6960373e26dc4d5ab2808eb02f12e07311600c3
pauloba <paulobacolobonb@gmail.com>
2022-09-24 20:23:57 -0500

    version2

--- contabiliza.cpp
+++ contabiliza.cpp
@@ -3,19 +3,19 @@ using namespace std;
 
 int main()
 {
-	int i=0,l;
-	float x,s=0;
-	cout<<"ingrese el limite l="; cin>>l;
+	int PJCB_i=0,PJCB_l;
+	float PJCB_x,PJCB_s=0;
+	cout<<"ingrese el limite PJCB_l="; cin>>PJCB_l;
 	do{
 		
-	cout<<"ingrese el numero x="; cin>>x;
-	i=i+1;
-	s=s+x;
+	cout<<"ingrese el numero PJCB_x="; cin>>PJCB_x;
+	PJCB_i=PJCB_i+1;
+	PJCB_s=PJCB_s+PJCB_x;
 
 
 
-	}while(i<l);
-	cout<<"Se ingresaron "<<l<<" numeros "<< "que sumaron "<<s<<endl;
+	}while(PJCB_i<PJCB_l);
+	cout<<"Se ingresaron "<<PJCB_l<<" numeros "<< "que sumaron "<<PJCB_s<<endl;
 	return 0;
 
 
